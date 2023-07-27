e643bad8d18f8992e2b09e6dd83d33281795de21
Ayush Raj <ayushrajshr44@gmail.com>
2023-07-27 17:55:10 +0530

    header and map api

---
+++ src/components/Map/styles.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable react-refresh/only-export-components */
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles(() => ({
+  paper: {
+    padding: '10px', display: 'flex', flexDirection: 'column', justifyContent: 'center', width: '100px',
+  },
+  mapContainer: {
+    height: '85vh', width: '100%',
+  },
+  markerContainer: {
+    position: 'absolute', transform: 'translate(-50%, -50%)', zIndex: 1, '&:hover': { zIndex: 2 },
+  },
+  pointer: {
+    cursor: 'pointer',
+  },
+}));
---
+++ src/components/PlaceDetails/styles.jsx
@@ -0,0 +1,14 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+// eslint-disable-next-line react-refresh/only-export-components
+export default makeStyles(() => ({
+  chip: {
+    margin: '5px 5px 5px 0',
+  },
+  subtitle: {
+    display: 'flex', alignItems: 'center', justifyContent: 'space-between', marginTop: '10px',
+  },
+  spacing: {
+    display: 'flex', alignItems: 'center', justifyContent: 'space-between',
+  },
+}));
